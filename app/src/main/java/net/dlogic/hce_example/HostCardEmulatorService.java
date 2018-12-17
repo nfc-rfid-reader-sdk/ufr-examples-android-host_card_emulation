1aa672b61751f153dcb76808dd35784f32f1f0b1
developer4 <support@d-logic.rs>
2018-12-17 11:14:55 +0100

    Cosmetic changes only

--- app/src/main/java/net/dlogic/hce_example/HostCardEmulatorService.java
+++ app/src/main/java/net/dlogic/hce_example/HostCardEmulatorService.java
@@ -9,14 +9,14 @@ import net.dlogic.hce_example.Utils;
 public class HostCardEmulatorService extends HostApduService {
 
     class Const {
-        public static final String TAG = "Host Card Emulator";
-        public static final String STATUS_SUCCESS = "9000";
-        public static final String STATUS_FAILED = "6F00";
-        public static final String CLA_NOT_SUPPORTED = "6E00";
-        public static final String INS_NOT_SUPPORTED = "6D00";
-        public static final String AID = "F00102030405"; // Chose your own AID (RID + PIX)
-        public static final String SELECT_INS = "A4";
-        public static final String DEFAULT_CLA = "00";
+        static final String TAG = "Host Card Emulator";
+        static final String STATUS_SUCCESS = "9000";
+        static final String STATUS_FAILED = "6F00";
+        static final String CLA_NOT_SUPPORTED = "6E00";
+        static final String INS_NOT_SUPPORTED = "6D00";
+        static final String AID = "F00102030405"; // Chose your own AID (RID + PIX)
+        static final String SELECT_INS = "A4";
+        static final String DEFAULT_CLA = "00";
     }
 
     @Override
@@ -40,7 +40,7 @@ public class HostCardEmulatorService extends HostApduService {
             String ret_msg = "Digital Logic Ltd.";
 
             byte[] sw = Utils.hexStringToByteArray(Const.STATUS_SUCCESS);
-            byte[] ret = Utils.concatenateByteArrays(ret_msg.getBytes(), sw);;
+            byte[] ret = Utils.concatenateByteArrays(ret_msg.getBytes(), sw);
             return ret;
         } else {
             return Utils.hexStringToByteArray(Const.STATUS_FAILED);
